44f1f0fa9c2be60bd82a2f8a8c5889c9ecb64862
Elijah <elijahdj88@gmail.com>
2017-09-27 10:31:00 -0400

    This is the first commit!

---
+++ Lab01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            bool keepGoing = true;
+            while (keepGoing == true)
+            {
+                double length;
+                double width;
+
+                Console.WriteLine("Enter length:");
+                length = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter width:");
+                width = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("The area is " + length * width);
+                Console.WriteLine("The perimeter is " + 2 * (length + width));
+
+                string userChoice;
+                Console.WriteLine("Continue? (y/n)");
+                userChoice = Console.ReadLine();
+                if (userChoice == "n")
+                {
+                    keepGoing = false;
+                }
+            }
+        }
+    }
+}
